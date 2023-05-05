09533a63272327df989486e28939f66724feaff1
PaulsAV <lampteacher@hotmail.com>
2023-05-05 14:55:16 +0300

    fdfd

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from pywebio_battery import *
 from pywebio import *
+from pywebio.session import eval_js, run_js
 
+
+html = '''<input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">'''
+
+js = '''if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault()'''
 
 def application():
     def newfile():
         output.clear('panell')
-        input.file_upload('Выберите файлы', 'audio/*')
+        print(input.file_upload('Выберите файлы', 'audio/*'))
     output.put_markdown('# ПАНЕЛЬ ДИСТАНЦИОННОГО УПРАВЛЕНИЯ')
     output.put_scrollable(output.put_scope('files'))
     output.put_scope('panel').style('bottom: 10px; margin-left:20px;')
     output.put_scope('panall')
-    output.put_button('Новые файлы', onclick=newfile, scope='panell')
+    output.put_html(html)
+    run_js("""document.getElementById('fileElem').style.display = 'none'""")
+    output.put_button('Новые файлы', onclick=lambda: run_js(js))
+    data = {}
+    while len(data.keys()) == 0:
+        data = eval_js('''document.getElementById('fileElem').files''')
+    print(eval_js('''console.log(document.getElementById('fileElem').files)'''))
+    print(data)
+    output.toast(data)
 
     # output.put_text('Hello', scope='panel')
 
