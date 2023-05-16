4d57abea1cb44af4871047e51d331bb784b60427
PaulsAV <lampteacher@hotmail.com>
2023-05-16 09:34:47 +0300

    fdfd

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pywebio_battery import *
 from pywebio import *
 from pywebio.session import eval_js, run_js
 
-
 html = '''<input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">'''
 
 js = '''if (fileElem) {
@@ -10,25 +9,35 @@ js = '''if (fileElem) {
   }
   e.preventDefault()'''
 
+sock = """var xhr = new XMLHttpRequest();
+xhr.open('POST', '/upload', true);
+xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    console.log('Файл загружен!');
+  } else {
+    console.log('Произошла ошибка.');
+  }
+};
+var formData = new FormData();
+formData.append('file', file);
+xhr.send(formData);"""
+
+
 def application():
     def newfile():
-        output.clear('panell')
-        print(input.file_upload('Выберите файлы', 'audio/*'))
+        data = {}
+        while len(data.keys()) == 0:
+            data = eval_js('''document.getElementById('fileElem').files''')
+
     output.put_markdown('# ПАНЕЛЬ ДИСТАНЦИОННОГО УПРАВЛЕНИЯ')
     output.put_scrollable(output.put_scope('files'))
     output.put_scope('panel').style('bottom: 10px; margin-left:20px;')
-    output.put_scope('panall')
+    output.put_scope('panell')
     output.put_html(html)
     run_js("""document.getElementById('fileElem').style.display = 'none'""")
     output.put_button('Новые файлы', onclick=lambda: run_js(js))
-    data = {}
-    while len(data.keys()) == 0:
-        data = eval_js('''document.getElementById('fileElem').files''')
-    print(eval_js('''console.log(document.getElementById('fileElem').files)'''))
-    print(data)
-    output.toast(data)
-
-    # output.put_text('Hello', scope='panel')
+    newfile()
 
 
-start_server(application, host='192.168.137.1', port=8, debug=True)
+start_server(application, host='192.168.137.1', port=80, debug=True)
